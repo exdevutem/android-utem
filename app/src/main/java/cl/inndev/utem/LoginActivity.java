e626f4b4079644394bca8306eb32069ba6ec5025
Jorge Verdugo <jorgeverdugoch@gmail.com>
2018-03-06 17:30:34 -0300

    Se agrega menú lateral de navegación

--- app/src/main/java/cl/inndev/utem/LoginActivity.java
+++ app/src/main/java/cl/inndev/utem/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.pollo.utem;
+package cl.inndev.utem;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.loopj.android.http.*;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,15 +35,19 @@ public class LoginActivity extends AppCompatActivity {
 
     private void validar(String rut, String pass) {
         Mensaje = (TextView)findViewById(R.id.textView2);
-        if (rut.equals("19841526") && pass.equals("6c834")){
+        if (rut == null || rut.isEmpty() || pass == null || pass.isEmpty()) {
+            Mensaje.setText("No pueden haber campos vacíos");
+        } /*else if (rut.equals("19841526") && pass.equals("6c834")){
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.putExtra("USERNAME", rut);
             startActivity(intent);
-        } else if (rut == null || rut.isEmpty() || pass == null || pass.isEmpty()) {
-            Mensaje.setText("No pueden haber campos vacíos");
         } else {
             Mensaje.setText("Los datos son incorrectos");
-        }
+        }*/
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra("USERNAME", rut);
+        startActivity(intent);
+        finish();
     }
 
 
